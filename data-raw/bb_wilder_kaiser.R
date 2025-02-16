4cc655c3cc7dcf4db06967fcf535bfbe7fdaf73e
Jan Simson <git@simson.io>
2025-02-16 22:01:54 +0100

    Include a bounding box for wilder kaiser by default

---
+++ data-raw/bb_wilder_kaiser.R
@@ -0,0 +1,9 @@
+# Bounding box for wilder kaiser
+bb_wilder_kaiser <- c(
+  xmin = -347.87315,
+  ymin = 47.52787,
+  xmax = -347.57103,
+  ymax = 47.62884
+)
+
+usethis::use_data(bb_wilder_kaiser, overwrite = TRUE)
